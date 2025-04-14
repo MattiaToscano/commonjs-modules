73962e82fc699fa8d7c99192e41011856bd25391
MattiaToscano <mattia1997ct@gmail.com>
2025-04-14 13:27:33 +0200

    [UPD] Creata funzione per hobbies e inizializzata l'esportazione

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+//Inizializzo una funzione che accetta 3 parametri, honbbyOne, hobbyTwo e hobbyThree
+function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+    return `I miei hobby sono ${hobbyOne}, ${hobbyTwo} e ${hobbyThree}`;
+}
+console.log(hobbies);
+
+module.exports = hobbies;
